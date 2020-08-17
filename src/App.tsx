67e899a68d539902153e4cd050aae8113d2f4a26
Bersder3000 <B1747322151@outlook.com>
2020-08-17 14:22:23 +0800

    init2

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div style={{ color: '#5abebc' }}>2333</div>
+    );
+  }
+}
+
+export default App;
